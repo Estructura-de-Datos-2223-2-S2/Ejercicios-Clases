acbf50517d4ba7e975db9fda9655dee2b339c523
Antonio Guerra <anapoleon@applaudostudios.com>
2023-02-03 20:08:40 -0400

    ✨ Clase Listas circulares

---
+++ Semana 4/Miercoles/ListasCirculares/ListasCirculares/src/listascirculares/Main.java
@@ -0,0 +1,43 @@
+package listascirculares;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        ListaCircular list = new ListaCircular();
+        list.insertBegin(1);
+        list.insertFinal(2);
+        list.insertFinal(3);
+        list.insertFinal(4);
+        list.insertBegin(5);
+        list.printList();
+        list.insertInIndex(1, 8);
+        System.out.println("NUEVA LISTA 1");
+        list.printList();
+        list.insertInIndex(2, 20);
+        System.out.println("NUEVA LISTA 12");
+        list.printList();
+        list.insertInIndex(3, 12);
+        System.out.println("NUEVA LISTA 2");
+        list.printList();
+        list.insertInIndex(6, 15);
+        System.out.println("NUEVA LISTA 3");
+        list.printList();
+        System.out.println("DELETE ELEMENT");
+        /*list.deleteElement(8);
+        list.deleteElement(15);
+        list.deleteElement(15);*/
+        list.printList();
+        list.deleteBegin();
+        list.deleteFinal();
+        System.out.println("NUEVA LISTA DELETE");
+        list.printList();
+        System.out.println("NUEVA LISTA DELETE FINAL");
+        list.deleteInIndex(4);
+        list.printList();
+        System.out.println("NUEVA LISTA DELETE FINAL 2");
+        list.deleteInIndex(4);
+        list.printList();
+    }
+    
+}
---
+++ Semana 4/Miercoles/ListasCirculares/ListasCirculares/src/listascirculares/Nodo.java
@@ -0,0 +1,41 @@
+package listascirculares;
+
+public class Nodo {
+    private int element;
+    private Nodo next;
+    private Nodo previous;
+
+    public Nodo(int elemento) {
+        this.element = elemento;
+        this.next = null;
+        this.previous = null;
+    }
+
+    public int getElement() {
+        return element;
+    }
+
+    public void setElement(int elemento) {
+        this.element = elemento;
+    }
+
+    public Nodo getNext() {
+        return next;
+    }
+
+    public void setNext(Nodo siguiente) {
+        this.next = siguiente;
+    }
+
+    public Nodo getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(Nodo previous) {
+        this.previous = previous;
+    }
+    
+    
+    
+    
+}
